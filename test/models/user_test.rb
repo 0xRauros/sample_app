8df1ce21308c0dd39205f6c8b3850162d7085adf
0xrauros <aromopimou16@gmail.com>
2022-03-30 16:33:46 +0200

    Make a basic User model (including secure passwords)

---
+++ test/models/user_test.rb
@@ -0,0 +1,54 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+
+  def setup # Se ejecuta automaticamente antes de cada test
+    @user = User.new(name: "Bruce Wayne", email: "bwayne@bwinc.gt",
+                    password: "foobar", password_confirmation: "foobar")
+    # @user es variable de instancia disponible en todos los test
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "  "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    # emails are case unsensitive
+    # we validate with :case_sensitive user.rb User class
+    # duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+end
